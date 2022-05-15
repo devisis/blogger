2ca154a6dcc2d6c53c0906c844a0be497f4e0412
DevIsis <kitcher96@gmail.com>
2022-05-15 01:36:53 +0000

    Deployment Commit

--- blogstar/settings.py
+++ blogstar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['blogstar-2022.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['blogstar-2022.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -130,8 +130,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIR = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
